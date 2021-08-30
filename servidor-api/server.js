0f5a4382701be60419008c122b9cca72268b28ef
alanrente <alan.rente@gmail.com>
2021-08-30 15:44:39 -0300

    doido

--- servidor-api/server.js
+++ servidor-api/server.js
@@ -1,7 +1,7 @@
 var http = require('http')
     ,app = require('./config/express');
 
-const port = process.env.PORT || 8080
+const port = 8080
 
 http.createServer(app).listen(port, function() {
     console.log('Servidor escutando na porta: ' + this.address().port);
